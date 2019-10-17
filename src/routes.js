1739fd3082c87a24f3b829969dfdb832cc5cb237
Hin Wong <hin.wong@finddoc.com>
2019-10-17 17:43:49 +0800

    Mimic ios transition

--- src/routes.js
+++ src/routes.js
@@ -2,19 +2,33 @@ import React from 'react';
 import {
     Route,
 } from 'react-router-dom';
-import { AnimatedSwitch } from 'react-router-transition';
+import { AnimatedSwitch, spring } from 'react-router-transition';
 
 import style from './routes.module.scss';
 import HomePage from './pages/HomePage';
 import ProcessPage from './pages/ProcessPage';
 
+function mapStyles(styles) {
+    return {
+        transform: `translateX(${styles.offset}%)`,
+    };
+}
+
+function slide(val) {
+    return spring(val, {
+        stiffness: 200,
+        damping: 24,
+    });
+}
+
 const Routes = () => {
     return (
         <AnimatedSwitch
-            atEnter={{ opacity: 0 }}
-            atLeave={{ opacity: 0 }}
-            atActive={{ opacity: 1 }}
+            atEnter={{ offset: 100 }}
+            atLeave={{ offset: slide(-100) }}
+            atActive={{ offset: slide(0) }}
             className={style.switchWrapper}
+            mapStyles={mapStyles}
         >
             <Route
                 path="/process"
