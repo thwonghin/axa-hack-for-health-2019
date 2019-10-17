067b9e4f517c9d73332ccec7d36634e00fba3dd6
Hin Wong <hin.wong@finddoc.com>
2019-10-17 13:18:30 +0800

    Add folder sturcture

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+    return (<span>Hello World</span>);
+};
+
+export default Home;
