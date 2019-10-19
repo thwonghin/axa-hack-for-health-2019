f9cd35c950e72a70f3fd0fb7a133f957365bd25c
OnikurYH <onikur.chan@finddoc.com>
2019-10-19 15:27:28 +0800

    Basic phycho page layout

--- src/pages/PhychoPage/index.js
+++ src/pages/PhychoPage/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { Card, Media, Button } from 'react-bootstrap';
+
+import mildDepression from '../../assets/images/mild-depression.svg';
+import severeAnxiety from '../../assets/images/severe-anxiety.svg';
+
+import styles from './index.module.scss';
 
 const PhychoPage = () => {
     return (
-        <p>You are phycho</p>
+        <div>
+            <p className={styles.intro}>Based on our analysis, here are our insights</p>
+            <header className={styles.header}>
+                <h1>Mud Mud</h1>
+            </header>
+            <div className={styles.phychoList}>
+                <Card className={styles.phycho}>
+                    <Card.Body>
+                        <Media>
+                            <Media.Body>
+                                <div className={styles.phychoIntro}>You might have</div>
+                                <h2>Mild Depression</h2>
+                            </Media.Body>
+                            <img
+                                src={mildDepression}
+                                width={39}
+                                height={48}
+                                alt="Mild Depression"
+                            />
+                        </Media>
+                    </Card.Body>
+                </Card>
+                <Card className={styles.phycho}>
+                    <Card.Body>
+                        <Media>
+                            <Media.Body>
+                                <div className={styles.phychoIntro}>You might have</div>
+                                <h2>Mild Depression</h2>
+                            </Media.Body>
+                            <img
+                                src={severeAnxiety}
+                                width={39}
+                                height={48}
+                                alt="Severe Anxiety"
+                            />
+                        </Media>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div className={styles.cta}>
+                <Button className={styles.ctaButton}>CTA talk to chat bot?</Button>
+            </div>
+        </div>
     );
 };
 
