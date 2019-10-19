bb8ea878947f98b52f2966299e5fbebd6f20114a
OnikurYH <onikur.chan@finddoc.com>
2019-10-19 18:51:17 +0800

    Update home style

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 import homeBg from '../../assets/images/home-bg.svg';
 import style from './index.module.scss';
+
+import facebook from '../../assets/images/facebook.svg';
+import twitter from '../../assets/images/twitter.svg';
+import orangeArrow from '../../assets/images/orange-arrow.svg'
 
 import IgConnectButton from '../../components/IgConnectButton';
 
@@ -16,13 +21,23 @@ const Home = () => {
                 alt="Home BG"
             />
             <div className={style.content}>
-                <h1 className={style.heading}>Persononized your Health Assessment with just one click.</h1>
+                <h1 className={style.heading}>Understand<br/>Your<br/>Mental Health<br/>with just<br/>one click.</h1>
                 <p className={style.description}>Lôrem ipsum dolor sït àmet, vivëndùm àdversarium Lôrem ipsum dolor sït àmet, vivëndùm àdversarium Lôrem ipsum dolor sït àmet.</p>
                 <div
                     className={style.cta}
                 >
+                    <Button className={`${style.ctaButton} mr-3`} variant="light">
+                        <img src={facebook} width={34} height={34} alt="Facebook"/>
+                    </Button>
                     <IgConnectButton />
+                    <Button className={`${style.ctaButton} ml-3`} variant="light">
+                        <img src={twitter} width={34} height={34} alt="Twitter"/>
+                    </Button>
                 </div>
+            </div>
+            <div className={style.ctaDescription}>
+                <img className={style.ctaDescriptionArrow} src={orangeArrow} width={35} height={50} alt="Orange arrow" />
+                Select one of the following Social Media Platform to get your results!
             </div>
         </div>
     );
