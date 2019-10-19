5bb31879502efea0c3a4e0df6f9c84d78b8f26dd
Hin Wong <hin.wong@finddoc.com>
2019-10-19 12:01:52 +0800

    Add procwss page

--- src/components/MainNavbar/index.js
+++ src/components/MainNavbar/index.js
@@ -4,14 +4,20 @@ import { Navbar } from 'react-bootstrap';
 import style from './index.module.scss';
 
 import logo from '../../assets/images/axa-logo.png';
+import logo2x from '../../assets/images/axa-logo@2x.png';
+import logo3x from '../../assets/images/axa-logo@3x.png';
 import navbarButton from '../../assets/images/navbar-button.svg';
+import { usePushHistory } from '../../libs/hooks';
 
 export default function MainNavbar() {
+    const onClickLogo = usePushHistory('/');
+
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">
+        <Navbar className={style.navbar} bg="light" expand="lg" fixed="top">
+            <Navbar.Brand className={style.navbarBrand} onClick={onClickLogo}>
                 <img
                     src={logo}
+                    srcset={`${logo} 1x, ${logo2x} 2x, ${logo3x} 3x`}
                     width="87"
                     height="46"
                     className="d-inline-block align-top"
