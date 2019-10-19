656c6971444084ae8dfa0009bc30bcd5e5fee2b3
OnikurYH <onikur.chan@finddoc.com>
2019-10-19 23:14:05 +0800

    Update navbar and page margin top

--- src/components/MainNavbar/index.js
+++ src/components/MainNavbar/index.js
@@ -1,36 +1,23 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
 import style from './index.module.scss';
 
-import logo from '../../assets/images/axa-logo.png';
-import logo2x from '../../assets/images/axa-logo@2x.png';
-import logo3x from '../../assets/images/axa-logo@3x.png';
-import navbarButton from '../../assets/images/navbar-button.svg';
-import { usePushHistory } from '../../libs/hooks';
+import arrowLeft from '../../assets/images/arrow-left.svg';
+import times from '../../assets/images/times.svg';
 
 export default function MainNavbar() {
-    const onClickLogo = usePushHistory('/');
+    const history = useHistory();
 
     return (
         <Navbar className={style.navbar} bg="light" expand="lg" fixed="top">
-            <Navbar.Brand className={style.navbarBrand} onClick={onClickLogo}>
-                <img
-                    src={logo}
-                    srcset={`${logo} 1x, ${logo2x} 2x, ${logo3x} 3x`}
-                    width="87"
-                    height="46"
-                    className="d-inline-block align-top"
-                    alt="Logo"
-                />
-            </Navbar.Brand>
-            <Navbar.Toggle className={style.navbarButton} aria-controls="basic-navbar-nav">
-                <img
-                    src={navbarButton}
-                    alt="Button"
-                />
-            </Navbar.Toggle>
-            <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+            <Button className={`${style.actionButtons}`} variant="link" onClick={history.goBack}>
+                <img src={arrowLeft} width={21} height={29} alt="Back" />
+            </Button>
+            <Button className={`${style.actionButtons} ml-auto`} variant="link">
+                <img src={times} width={21} height={29} alt="Close" />
+            </Button>
         </Navbar>
     );
 }
