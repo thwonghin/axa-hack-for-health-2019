2e0b6045ebfb2904cef4f7902df145c84b089661
OnikurYH <OnikurYH@users.noreply.github.com>
2019-10-19 08:37:26 +0800

    Landing (#7)
    * Update Home style
    
    * Update styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import MainNavbar from './components/MainNavbar';
 import Routes from './routes';
 
 const App = () => (
     <BrowserRouter>
+        <MainNavbar />
         <Routes />
     </BrowserRouter>
 );
